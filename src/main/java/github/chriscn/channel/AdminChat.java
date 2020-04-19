4fc8c2abbb20dbbb046f6374f2874e0cdc119ed6
Christopher Nethercott <ccnethercott@gmail.com>
2020-04-19 20:48:20 +0100

    We are starting!!

---
+++ src/main/java/github/chriscn/channel/AdminChat.java
@@ -0,0 +1,15 @@
+package github.chriscn.channel;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+
+public class AdminChat implements CommandExecutor, Listener {
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
