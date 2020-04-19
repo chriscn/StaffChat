ce68530f21a7cbdc360a5dd6ea85d0ad4cf67b96
Christopher Nethercott <ccnethercott@gmail.com>
2020-04-19 21:42:55 +0100

    Created staff chat command

---
+++ src/main/java/github/chriscn/command/StaffChatCommand.java
@@ -0,0 +1,39 @@
+package github.chriscn.command;
+
+import github.chriscn.StaffChat;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class StaffChatCommand implements CommandExecutor {
+
+    StaffChat plugin;
+
+    public StaffChatCommand(StaffChat instance) {
+        this.plugin = instance;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender.hasPermission(plugin.staffChat)) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                sb.append(args[i]);
+            }
+
+            String msg = sb.toString();
+
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                if (plugin.inStaffChat.contains(p.getUniqueId())) {
+                    p.sendMessage("[STAFF] <" + sender.getName() + "> " + msg);
+                }
+            }
+        } else {
+            plugin.noPermission(sender);
+        }
+        return true;
+    }
+}
