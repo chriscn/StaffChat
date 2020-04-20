9a5c3d72e5b51eb6d2eabfbe34133f50f9987d5d
Christopher Nethercott <ccnethercott@gmail.com>
2020-04-20 18:38:50 +0100

    Added channel command

--- src/main/java/github/chriscn/command/ChannelCommand.java
+++ src/main/java/github/chriscn/command/ChannelCommand.java
@@ -1,9 +1,12 @@
 package github.chriscn.command;
 
 import github.chriscn.StaffChat;
+import github.chriscn.util.ChannelEnum;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 
 import java.util.List;
 
@@ -17,5 +20,35 @@ public class ChannelCommand implements TabCompleter {
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         return null;
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
+        if (!(sender instanceof Player)) {
+            plugin.notPlayer(sender);
+        } else {
+            Player player = (Player) sender;
+
+            if (plugin.playerChannel.containsKey(player.getUniqueId())) {
+                plugin.playerChannel.remove(player.getUniqueId());
+            }
+
+            switch (args[0].toLowerCase()) {
+                case "all":
+                    plugin.playerChannel.put(player.getUniqueId(), ChannelEnum.ALL);
+                    break;
+                case "staff":
+                    plugin.playerChannel.put(player.getUniqueId(), ChannelEnum.STAFF);
+                    break;
+                case "admin":
+                    plugin.playerChannel.put(player.getUniqueId(), ChannelEnum.ADMIN);
+                    break;
+                default:
+                    plugin.unknownChannel(sender);
+                    return false;
+            }
+
+            player.sendMessage(ChatColor.AQUA + "You have been switched to: " + plugin.playerChannel.get(player.getUniqueId()).getName());
+        }
+        return true;
     }
 }
