e784d9b9899daae0a47995e5b9b79d353e39dab9
Christopher Nethercott <ccnethercott@gmail.com>
2020-04-21 10:27:56 +0100

    Create ListChannelCommand

---
+++ src/main/java/github/chriscn/command/ListChannelCommand.java
@@ -0,0 +1,13 @@
+package github.chriscn.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ListChannelCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        
+        return false;
+    }
+}
