84c985c2abf51cc9855e15447d755cb2ec4ca2f0
Christopher Nethercott <ccnethercott@gmail.com>
2020-07-22 21:36:35 +0100

    Removed unused imports

--- src/main/java/github/chriscn/StaffChat.java
+++ src/main/java/github/chriscn/StaffChat.java
@@ -1,24 +1,10 @@
 package github.chriscn;
 
-import github.chriscn.command.ChannelCommand;
-import github.chriscn.command.StaffChatCommand;
-import github.chriscn.events.PlayerChatEvent;
-import github.chriscn.util.ChannelEnum;
-import net.md_5.bungee.api.ChatColor;
-import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.permissions.Permission;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.UUID;
 import java.util.logging.Level;
 
 public final class StaffChat extends JavaPlugin {
