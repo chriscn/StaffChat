6e5b8ff424900d327b0c9382c74f0b3de63e917c
Christopher Nethercott <ccnethercott@gmail.com>
2020-07-28 17:13:35 +0100

    Create ChatCommand allowing sending of one message to a channel

---
+++ src/main/java/github/chriscn/command/ChatCommand.java
@@ -0,0 +1,19 @@
+package github.chriscn.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+
+import java.util.List;
+
+public class ChatCommand implements TabExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
+        return null;
+    }
+}
