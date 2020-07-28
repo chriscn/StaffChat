e9cc537767aab2b891b3e36cecadeed1ae89969e
Christopher Nethercott <ccnethercott@gmail.com>
2020-07-28 11:40:28 +0100

    Testing the debug command

--- src/main/java/github/chriscn/command/DebugCommand.java
+++ src/main/java/github/chriscn/command/DebugCommand.java
@@ -8,14 +8,31 @@ import org.bukkit.command.CommandSender;
 
 public class DebugCommand implements CommandExecutor {
 
-    StaffChat plugin = new StaffChat();
+    StaffChat plugin;
+    public DebugCommand(StaffChat instance) {
+        this.plugin = instance;
+    }
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        commandSender.sendMessage("testing");
+
         plugin.masterChannels.forEach((uuid, channel) -> {
             commandSender.sendMessage("Player " + Bukkit.getPlayer(uuid).getDisplayName() + " Channel " + channel);
         });
 
-        return false;
+        plugin.channelPermissions.forEach((channel, permission) -> {
+            commandSender.sendMessage("Channel " + channel + " permssion " + permission.getName());
+        });
+
+        plugin.config.getKeys(true).forEach(key -> {
+            commandSender.sendMessage(key);
+        });
+
+        plugin.config.getConfigurationSection("channel").getKeys(true).forEach(key -> {
+            commandSender.sendMessage(key);
+        });
+
+        return true;
     }
 }
