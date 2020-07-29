0e67448385a058e4b262404f3dd4989bde3d0049
Christopher Nethercott <ccnethercott@gmail.com>
2020-07-29 13:41:47 +0100

    beginning with some debugging

--- src/main/java/github/chriscn/command/PluginCommand.java
+++ src/main/java/github/chriscn/command/PluginCommand.java
@@ -5,9 +5,11 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
 
 import java.util.List;
 
+// This is just a debug / administrator command
 public class PluginCommand implements TabExecutor {
 
     StaffChat plugin;
@@ -16,8 +18,26 @@ public class PluginCommand implements TabExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        return false;
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
+        if (commandSender instanceof Player) {
+            Player player = (Player) commandSender;
+            if (args.length == 0) {
+                return false;
+            } else if (args.length == 1) {
+                String firstArgument = args[0].toLowerCase();
+                if (firstArgument == "channels") {
+                    plugin.virtualChannels.values().forEach(vc -> {
+                        player.sendMessage(vc.getDebug());
+                    });
+                }
+            } else {
+                return false;
+            }
+            return false;
+        } else {
+            commandSender.sendMessage(plugin.notPlayer);
+            return true;
+        }
     }
 
     @Override
